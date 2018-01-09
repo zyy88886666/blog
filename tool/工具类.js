580a002bde5d4a8b366a5bef2a579d08a6b2f22f
zhaoyuanyuan <swpu_zyy@163.com>
2018-01-09 09:23:35 +0800

    add call,apply,bind

--- tool/工具类.js
+++ tool/工具类.js
@@ -75,3 +75,68 @@ function removeClass( elements,cName ){
 }; 
 
 /////////////////////////////////////////////////////
+
+
+
+
+//模拟call方法
+//
+Function.prototype.call1 = function (context){
+    var context = context || window ;  //判断是否传入参数
+
+    context.fn = this;  //将值复制为函数的属性,新增属性
+    
+    var args = [];
+    for(var i=1 ; i<arguments.length ; i++ ){
+        args.push('arguments[' + i + ']');
+    }
+
+   var resule = eval('context.fn(' + args + ')');  // 执行方法
+
+    delete context.fn;  //删除属性
+    return resule;    
+}
+
+
+//模拟apply的方法
+Function.prototype.apply = function (context, arr) {
+    var context = Object(context) || window;
+    context.fn = this;
+
+    var result;
+    if (!arr) {
+        result = context.fn();
+    }
+    else {
+        var args = [];
+        for (var i = 0, len = arr.length; i < len; i++) {
+            args.push('arr[' + i + ']');
+        }
+        result = eval('context.fn(' + args + ')')
+    }
+
+    delete context.fn
+    return result;
+}
+
+
+//模拟bind的返回函数的模拟实现
+//
+Function.prototype.bind1=function(context){
+    var self = this ;
+    return function(){
+        return self.apply(context);
+    }
+}
+
+//传参的模拟实现
+
+Function.prototype.bind2=function(context){
+    var self = this;
+
+    var args = Array.prototype.slice.call(arguments,1); //获取bind2函数从第二个参数到最后一个参数
+    return function(){
+        var bindargs = Array.portotype.slice.call(arguments);//这个时候的arguments是指bind返回的函数传入的参数
+        return self.apply(context,args.concat(bindargs));
+    }
+}
